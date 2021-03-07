07443d8a500fa9e11f33893b801490ac04913131
James Prickitt <jwprickitt@gmail.com>
2021-03-07 16:40:47 -0500

    events update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 class Event{
-    constructor(eventcode, eventtype, eventname, eventstartdate, eventenddate) {
+    constructor(eventid, eventcode, eventtype, eventname, eventstartdate, eventenddate) {
+        this._eventid = eventid
         this._eventcode = eventcode
+        this._eventtype = eventtype
         this._eventname = eventname
         this._eventstartdate = eventstartdate
         this._eventenddate = eventenddate
+    }
+    get eventid(){
+        return this._eventid
     }
     get eventcode(){
         return this._eventcode
@@ -20,13 +25,18 @@ class Event{
     get eventenddate(){
         return this._eventenddate
     }
+    showEventDetails() {
+        let eventdetails = [this._eventid, this._eventcode, this._eventtype, this._eventname, this._eventstartdate, this._eventenddate]
+        // console.log(`${this._eventid}, ${this._eventcode}, ${this._eventtype}, ${this._eventname}, ${this._eventstartdate}, ${this._eventenddate}`)
+        console.log(eventdetails)
+    }
     msgWelcome(){
         console.log(`Welcome to ${this._eventname}!`)
     }
 }
 class OnsiteEvent extends Event {
-    constructor(eventcode, eventtype, eventname, eventstartdate, eventenddate, eventvenue, eventcity, eventstate, eventcountry){
-        super(eventcode, eventtype, eventname, eventstartdate, eventenddate)
+    constructor(eventid, eventcode, eventtype, eventname, eventstartdate, eventenddate, eventvenue, eventcity, eventstate, eventcountry){
+        super(eventid, eventcode, eventtype, eventname, eventstartdate, eventenddate)
         this._eventvenue = eventvenue
         this._eventcity = eventcity
         this._eventstate = eventstate
@@ -46,8 +56,8 @@ class OnsiteEvent extends Event {
     }
 }
 class VirtualEvent extends Event{
-    constructor(eventcode, eventtype, eventname, eventstartdate, eventenddate, availablepostevent){
-        super(eventcode, eventtype, eventname, eventstartdate, eventenddate)
+    constructor(eventid, eventcode, eventtype, eventname, eventstartdate, eventenddate, availablepostevent){
+        super(eventid, eventcode, eventtype, eventname, eventstartdate, eventenddate)
         this._availablepostevent = availablepostevent
     }
     get availablepostevent(){
@@ -55,11 +65,12 @@ class VirtualEvent extends Event{
     }
 }
 
-let pack0921 = new OnsiteEvent('pack0921', 'onsite','International Packaging Expo 2021','9/1/2021','9/15/2021','Las Vegas Convention Center','Las Vegas','NV','United States')
-let spie1021 = new VirtualEvent('spie1021','virtual','2021 SPIE Engineering Expo','10/15/2021','10/20/2021',true)
+let pack0921 = new OnsiteEvent(0,'pack0921', 'onsite','International Packaging Expo 2021','9/1/2021','9/15/2021','Las Vegas Convention Center','Las Vegas','NV','United States')
+let spie1021 = new VirtualEvent(1,'spie1021','virtual','2021 SPIE Engineering Expo','10/15/2021','10/20/2021',true)
 
 let events = [pack0921,spie1021]
 
 for( a of events ){
-    a.msgWelcome()
+    a.showEventDetails()
 }
+
