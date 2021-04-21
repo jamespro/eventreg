b461a04a9c4818b7068f0023efe3824635d0d16b
James Prickitt <jwprickitt@gmail.com>
2021-04-20 23:40:00 -0400

    undo bug

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,10 @@
 module.exports = {
     getIndex: (req, res) => {
         res.render('index.ejs')
-        db.collection('events').find().sort({eventid:-1}).toArray()
-            .then(data => {
-            res.render('index.ejs',{events:data})
-            })
-        .catch(error => console.error(error))
+        // db.collection('events').find().sort({eventid:-1}).toArray()
+        //     .then(data => {
+        //     res.render('index.ejs',{events:data})
+        //     })
+        // .catch(error => console.error(error))
     }
 }
