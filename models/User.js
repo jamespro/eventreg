cb7fe19aad5e04040d4e34e104303ad90744f92f
James Prickitt <jwprickitt@gmail.com>
2021-05-31 11:48:56 -0400

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  address1: {
+    type: String,
+    required: true,
+  },
+  address2: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: false,
+  },
+  zipcode: {
+    type: String,
+    required: false,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  useAddressForPaymentDetails: {
+      type: Boolean,
+      default: false
+  },
+    createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
