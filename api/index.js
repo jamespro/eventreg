9bf4fcc03a2eb645185db938ffbdaafd8cde94f6
James Prickitt <jwprickitt@gmail.com>
2021-05-31 13:10:37 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const completeApi = require("./complete");
 const orderApi = require("./order");
 // const registerApi = require("./register");
 // const paymentApi = require("./payment");
 
 const router = express.Router();
 
+router.use(completeApi);
 router.use(orderApi);
 // router.use(registerApi);
 // router.use(paymentApi);
